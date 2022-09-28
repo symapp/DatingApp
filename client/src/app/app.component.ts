b5923bb910a774ec3181f03e9c8edbf34e1421ad
therealsymap <90852013+therealsymap@users.noreply.github.com>
2022-09-28 13:47:35 +0200

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { User } from "./models/user";
+import { AccountService } from "./_services/account.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    this.getUsers();
+  constructor(private accountService: AccountService) {
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
+  ngOnInit() {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem("user"));
+    this.accountService.setCurrentUser(user);
   }
 }
