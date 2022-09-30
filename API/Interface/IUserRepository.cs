e428b4e936d562084c8015777790be5fb894fd8f
therealsymap <90852013+therealsymap@users.noreply.github.com>
2022-09-30 17:56:11 +0200

    End of section 8

---
+++ API/Interface/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.DTO;
+using API.Entities;
+
+namespace API.Interface;
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+
+    Task<bool> SaveAllAsync();
+
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+
+    Task<AppUser> GetUserByIdAsync(int id);
+
+    Task<AppUser> GetUserByUsernameAsync(string username);
+
+    Task<IEnumerable<MemberDto>> GetMembersAsync();
+
+    Task<MemberDto> GetMemberAsync(string username);
+}
