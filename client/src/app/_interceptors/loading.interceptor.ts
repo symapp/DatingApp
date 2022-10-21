8fac8516c7c8555d852182e9b2f9766e7c9ded5e
therealsymap <90852013+therealsymap@users.noreply.github.com>
2022-10-21 12:16:42 +0200

    Section 19 - pre switch to postgres

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -13,7 +13,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(200),
       finalize(() => {
         this.busyService.idle();
       })
